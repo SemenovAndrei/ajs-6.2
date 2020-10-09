0c9f1e5287df7240e1ef5bec09d37b886ea890c1
Andrey Semenov <andss82@gmail.com>
2020-10-09 23:58:46 +0300

    add test

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,28 @@
 /* eslint-disable no-console */
-import orderByProps from './orderbyprops';
+import getSpecial from './getspecial';
 
-const obj = {
-  name: 'мечник',
-  health: 10,
-  level: 2,
-  attack: 80,
-  defence: 40,
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
 };
 
 
-console.log(orderByProps(obj, ['name', 'level']));
+console.log(getSpecial(character));
---
+++ src/js/getspecial.js
@@ -0,0 +1,23 @@
+function getSpecial(obj) {
+  const arr = [];
+
+  obj.special.forEach((item) => {
+    const {
+      id,
+      name,
+      icon,
+      description = 'Описание недоступно',
+    } = item;
+
+    arr.push({
+      id,
+      name,
+      icon,
+      description,
+    });
+  });
+
+  return arr;
+}
+
+export default getSpecial;
